2ee9a43bc4e170734cc763078592ff1499795b72
UKRoo <ivashko.dmytro@gmail.com>
2019-11-01 23:58:57 +0200

    1 done

---
+++ main.js
@@ -0,0 +1,32 @@
+// let age = prompt("Введiть свiй вік?");
+
+// if (age < 12) {
+//     console.log("Дитина");
+// } else if (age <= 18) {
+//     console.log("Підліток");
+// } else if (age <= 60) {
+//     console.log("Дорослий");
+// } else {
+//     console.log("Пенсіонер");
+// }
+
+
+
+// let key = prompt("Введіть число від 0 до 9");
+
+
+// if (key == 1) {
+//     console.log("!");
+// } else if (key == 2) {
+//     console.log("@");
+// } else if (key == 3) {
+//     console.log("#");
+// } else if (key == 4) {
+//     console.log("$");
+// } else if (key == 5) {
+//     console.log("%");
+// } else {
+//     console.log("аналогічно");
+// }
+
+
